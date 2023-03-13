7da54beffcfd887535fa1a9c914e676c66a73cf1
fengyukun <fengyukunfyk@sina.com>
2023-03-13 15:35:49 +0800

    removemeal

--- src/plugins/botConfig.py
+++ src/plugins/botConfig.py
@@ -32,6 +32,11 @@ async def _(event: Event, message: Message = EventMessage()):
             await addmeal(strings[3],0)
         if strings[2] == "二区":
             await addmeal(strings[3], 1)
+    if strings[1] == "removemeal":
+        if strings[2] == "一区":
+            await removemeal(strings[3],0)
+        if strings[2] == "二区":
+            await removemeal(strings[3], 1)
 
 
 async def addmeal(meal: string,campus:int):
@@ -53,5 +58,23 @@ async def addmeal(meal: string,campus:int):
         jsonStr.encode('unicode_escape').decode("unicode-escape")
         f.write(jsonStr)
 
+async def removemeal(meal: string,campus:int):
+    if system() == "Windows":
+        dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
+    else:
+        dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
 
+    with open(dinePath, 'r',encoding='utf-8') as f:
+        data = json.load(f)
+
+    if data['campus'][campus].count(meal) == 0:
+        await cfg.send("删除失败,菜单没有这个菜喵")
+    else:
+        data['campus'][campus].remove(meal)
+        await cfg.send(f"{meal}已经从{'一区' if campus ==0 else '二区'}删掉了喵!")
+
+    with open(dinePath, 'r+',encoding='utf-8') as f:
+        jsonStr = json.dumps(data)
+        jsonStr.encode('unicode_escape').decode("unicode-escape")
+        f.write(jsonStr)
 
