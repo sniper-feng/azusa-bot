12645efe5c39700cca520938b663fefe0f8682e7
sniper-feng <fengyukunfyk@sina.com>
2023-03-13 23:24:17 +0800

    add menu and help

--- src/plugins/dine.py
+++ src/plugins/dine.py
@@ -20,7 +20,36 @@ else:
 
 with open(dinePath, 'r', encoding="utf-8") as f:
     dineList = json.load(f)
+
 dine = on_regex(r"(一|二)区吃什么")
+sendDineList = on_regex(r".{2}菜单")
+
+list_name = ["一区", "二区", "超星", "香坊", "哈西", "百盛", "阿城", "江一", "江二"]
+
+@sendDineList.handle()
+async def _(event: Event, message: Message = EventMessage()):
+    if system() == "Windows":
+        dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
+    else:
+        dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
+    with open(dinePath, 'r', encoding="utf-8") as f:
+        dineList = json.load(f)
+    name = event.get_plaintext()[0:2]
+    if list_name.count(name) == 0:
+        await sendDineList.send(f"根本没有{name}这家店喵...这边建议您吃\n锤子\n锤子\n锤子\n喵!")
+    else:
+        index = list_name.index(name)
+        listStr = f"群友在{name}登录过的吃的有:\n"
+        if index <=1:
+            for dish in dineList['campus'][index]:
+                listStr += dish + "\n"
+        else:
+            for dish in dineList['shop'][index-2]:
+                listStr += dish + "\n"
+        listStr +="喵!"
+        await sendDineList.send(listStr)
+
+
 
 
 @dine.handle()
--- src/plugins/public.py
+++ src/plugins/public.py
@@ -18,15 +18,19 @@ help = on_command('help')
 @help.handle()
 async def _(bot: Bot, event: Event, state: T_State):
     help_str = '''可用命令如下：
-今日舞萌 查看今天的舞萌运势
-XXXmaimaiXXX什么 随机一首歌
-随个[dx/标准][绿黄红紫白]<难度> 随机一首指定条件的乐曲
-查歌<乐曲标题的一部分> 查询符合条件的乐曲
-[绿黄红紫白]id<歌曲编号> 查询乐曲信息或谱面信息
-<歌曲别名>是什么歌 查询乐曲别名对应的乐曲
-定数查歌 <定数>  查询定数对应的乐曲
-定数查歌 <定数下限> <定数上限>
-分数线 <难度+歌曲id> <分数线> 详情请输入“分数线 帮助”查看'''
+help | 查看帮助文档(别看 还没改 看了也没用)
+课题列表    | 查看当前课题
+课题id<id>    | 查看课题细节
+魅力冰城    | 查看哈尔滨机厅的人数(由群聊汇报)
+<机厅>几   | 查看该机厅人数
+<机厅><人数>    | 上报该机厅人数
+[一/二]区吃什么   |  在哈工大找吃的
+<机厅>吃什么 |  在机厅找吃的
+<机厅>菜单  |  查看这个地方所有的吃的
+botconfig addmeal/removemeal 一区/二区 <食物> | 给工大菜单加菜/删菜
+botconfig addmeal/removemeal <机厅> <食物> | 给机厅菜单加菜/删菜
+梓喵可爱 | 你猜
+'''
     await help.send(Message([
         MessageSegment("image", {
             "file": f"base64://{str(image_to_base64(text_to_image(help_str)), encoding='utf-8')}"
