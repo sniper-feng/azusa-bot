d1516ebb6da232d0575b3a155f5e1ed3ae8e97fa
sniper-feng <fengyukunfyk@sina.com>
2023-03-22 10:57:17 +0800

    add alias to quote

--- src/plugins/sendQuote.py
+++ src/plugins/sendQuote.py
@@ -1,4 +1,5 @@
 import json
+import string
 import time
 
 from nonebot import on_command, on_regex
@@ -15,20 +16,27 @@ import re
 
 from nonebot import require
 from nonebot.plugin.on import on_fullmatch, on_regex, on_command
+
 if system() == "Windows":
     quotePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\quote.json"
 else:
     quotePath = "/home/sniperpigeon/bot/azusa-bot/prop/quote.json"
-
+if system() == "Windows":
+    aliasPath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\alias.json"
+else:
+    aliasPath = "/home/sniperpigeon/bot/azusa-bot/prop/alias.json"
 
 addQuote = on_command("入典")
+
+
 @addQuote.handle()
 async def _(event: Event, message: Message = EventMessage()):
-    strs = event.get_plaintext().split(" ", 3)
+    strs = event.get_plaintext().split(" ", 2)
     with open(quotePath, 'r', encoding="utf-8") as f:
         quoteList = json.load(f)
-    if strs[1] in quoteList.keys():
-        quoteList[strs[1]].append(strs[2])
+    if not getNameFromList(strs[1], quoteList.keys()) is None:
+        name = getNameFromList(strs[1], quoteList.keys())
+        quoteList[name].append(strs[2])
         with open(quotePath, 'w', encoding="utf-8") as f:
             jsonStr = json.dumps(quoteList)
             jsonStr.encode('unicode_escape').decode("unicode-escape")
@@ -37,23 +45,44 @@ async def _(event: Event, message: Message = EventMessage()):
         await addQuote.send("这个人不在名人堂呢")
 
 
+# 加入名人堂 车乃 alias=何大东
 addPerson = on_command("加入名人堂")
+
+
 @addPerson.handle()
 async def _(event: Event, message: Message = EventMessage()):
     strs = event.get_plaintext().split(" ", 2)
     with open(quotePath, 'r', encoding="utf-8") as f:
         quoteList = json.load(f)
-    if strs[1] in quoteList.keys():
-        await addQuote.send("此人已在名人堂")
+    # 普通添加
+    if len(strs) == 2:
+        if strs[1] in quoteList.keys():
+            await addQuote.send("此人已在名人堂")
+        else:
+            quoteList[strs[1]] = []
+            with open(quotePath, 'w', encoding="utf-8") as f:
+                jsonStr = json.dumps(quoteList)
+                jsonStr.encode('unicode_escape').decode("unicode-escape")
+                f.write(jsonStr)
     else:
-        quoteList[strs[1]] = []
-        with open(quotePath, 'w', encoding="utf-8") as f:
-            jsonStr = json.dumps(quoteList)
-            jsonStr.encode('unicode_escape').decode("unicode-escape")
-            f.write(jsonStr)
+        # 添加别名
+        alias = strs[2][6:]  # 别名
+        if getNameFromList(strs[1], quoteList.keys()) is None:
+            await addQuote.send("此人不在名人堂")
+        else:
+            trueName = getNameFromList(strs[1], quoteList.keys())
+            with open(aliasPath, 'r', encoding="utf-8") as f:
+                aliasList = json.load(f)
+            aliasList[alias] = trueName
+            with open(aliasPath, 'w', encoding="utf-8") as f:
+                jsonStr = json.dumps(aliasList)
+                jsonStr.encode('unicode_escape').decode("unicode-escape")
+                f.write(jsonStr)
 
 
 checkPerson = on_fullmatch("查看名人堂")
+
+
 @checkPerson.handle()
 async def _(event: Event, message: Message = EventMessage()):
     with open(quotePath, 'r', encoding="utf-8") as f:
@@ -63,20 +92,37 @@ async def _(event: Event, message: Message = EventMessage()):
         sendstring += key + "\n"
     await checkPerson.send(sendstring)
 
+
 sendQuote = on_command("爆典")
+
+
 @sendQuote.handle()
 async def _(event: Event, message: Message = EventMessage()):
-    strs = event.get_plaintext().split(" ", 2)
+    strs = event.get_plaintext().split(" ", 1)
     with open(quotePath, 'r', encoding="utf-8") as f:
         quoteList = json.load(f)
 
-    if strs[1] in quoteList.keys():
-        quotes = quoteList[strs[1]]
+    if not getNameFromList(strs[1], quoteList.keys()) is None:
+        name = getNameFromList(strs[1], quoteList.keys())
+        quotes = quoteList[name]
         if len(quotes) == 0:
             await addQuote.send("这个人没有典呢")
         else:
-            index = random.randint(0, len(quotes)-1)
+            index = random.randint(0, len(quotes) - 1)
             quote = quotes[index]
-            await sendQuote.send(f"\"{quote}\"\n\n      ————{strs[1]}")
+            await sendQuote.send(f"\"{quote}\"\n\n      ————{name}")
     else:
         await addQuote.send("此人不在名人堂")
+
+
+# 如果不存在返回None
+def getNameFromList(inputName: string, nameList: Any) -> string:
+    if inputName in nameList:
+        return inputName
+    else:
+        with open(aliasPath, 'r', encoding="utf-8") as f:
+            aliasList = json.load(f)
+        if inputName in aliasList:
+            return aliasList[inputName]
+        else:
+            return None
