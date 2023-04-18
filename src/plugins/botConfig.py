f76cd1a96393a678ba898947917dc49342f4bdb7
sniper-feng <fengyukunfyk@sina.com>
2023-04-19 00:41:48 +0800

    Reconstruct the code

--- src/plugins/botConfig.py
+++ src/plugins/botConfig.py
@@ -25,42 +25,39 @@ if system() == "Windows":
 else:
     blacklistPath = "/home/sniperpigeon/bot/azusa-bot/prop/blacklist.json"
 cfg = on_command("botconfig")
+if system() == "Windows":
+    dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
+else:
+    dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
 
-shop_name = ["超星", "香坊", "哈西", "百盛", "阿城", "江一", "江二", "江北"]
+with open(dinePath, 'r', encoding="utf-8") as f:
+    dineList = json.load(f)
+shop_name = list(dineList.keys())
 block_admin = [1332019406, 824889294, 2530342608]
 super_admin = [1332019406]
 
+
 @cfg.handle()
 async def _(event: Event, message: Message = EventMessage()):
+    with open(blacklistPath, "r", encoding="utf-8") as f:
+        blackList = json.load(f)
     strings = event.get_plaintext().split(" ")
     # 格式 botconfig addmeal 一区/二区/机厅    吃什么
     #       0           1       2       3
     if strings[1] == "addmeal":
-        with open(blacklistPath, "r", encoding="utf-8") as f:
-            blackList = json.load(f)
         qqid = int(event.get_user_id())
         if qqid in blackList:
             await cfg.send("宝宝，你也配用？")
             return
-        if strings[2] == "一区":
-            await addmeal(strings[3], 0)
-        elif strings[2] == "二区":
-            await addmeal(strings[3], 1)
-        elif shop_name.count(strings[2]) == 1:
-            await addmeal_shop(strings[3], shop_name.index(strings[2]))
+        if shop_name.count(strings[2]) == 1:
+            await addmeal(strings[3], strings[2])
     if strings[1] == "removemeal":
-        with open(blacklistPath, "r", encoding="utf-8") as f:
-            blackList = json.load(f)
         qqid = int(event.get_user_id())
         if qqid in blackList:
             await cfg.send("宝宝，你也配用？")
             return
-        if strings[2] == "一区":
-            await removemeal(strings[3], 0)
-        if strings[2] == "二区":
-            await removemeal(strings[3], 1)
-        elif shop_name.count(strings[2]) == 1:
-            await removemeal_shop(strings[3], shop_name.index(strings[2]))
+        if shop_name.count(strings[2]) == 1:
+            await removemeal(strings[3], strings[2])
     if strings[1] == "removeBlock":
         if int(event.get_user_id()) in block_admin:
             with open(blacklistPath, "r", encoding="utf-8") as f:
@@ -72,48 +69,48 @@ async def _(event: Event, message: Message = EventMessage()):
                 return
 
 
-async def addmeal(meal: string, campus: int):
-    if system() == "Windows":
-        dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
-    else:
-        dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
+# async def addmeal(meal: string, campus: int):
+#     if system() == "Windows":
+#         dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
+#     else:
+#         dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
+#
+#     with open(dinePath, 'r', encoding='utf-8') as f:
+#         data = json.load(f)
+#
+#     if data['campus'][campus].count(meal) > 0:
+#         await cfg.send("添加失败,这个菜已经存在了喵")
+#     else:
+#         data['campus'][campus].append(meal)
+#         await cfg.send(f"{meal}已经加到{'一区' if campus == 0 else '二区'}了喵!")
+#     with open(dinePath, 'w', encoding='utf-8') as f:
+#         jsonStr = json.dumps(data)
+#         jsonStr.encode('unicode_escape').decode("unicode-escape")
+#         f.write(jsonStr)
+#
+#
+# async def removemeal(meal: string, campus: int):
+#     if system() == "Windows":
+#         dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
+#     else:
+#         dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
+#
+#     with open(dinePath, 'r', encoding='utf-8') as f:
+#         data = json.load(f)
+#
+#     if data['campus'][campus].count(meal) == 0:
+#         await cfg.send("删除失败,菜单没有这个菜喵")
+#     else:
+#         data['campus'][campus].remove(meal)
+#         await cfg.send(f"{meal}已经从{'一区' if campus == 0 else '二区'}删掉了喵!")
+#
+#     with open(dinePath, 'w', encoding='utf-8') as f:
+#         jsonStr = json.dumps(data)
+#         jsonStr.encode('unicode_escape').decode("unicode-escape")
+#         f.write(jsonStr)
+#
 
-    with open(dinePath, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-
-    if data['campus'][campus].count(meal) > 0:
-        await cfg.send("添加失败,这个菜已经存在了喵")
-    else:
-        data['campus'][campus].append(meal)
-        await cfg.send(f"{meal}已经加到{'一区' if campus == 0 else '二区'}了喵!")
-    with open(dinePath, 'w', encoding='utf-8') as f:
-        jsonStr = json.dumps(data)
-        jsonStr.encode('unicode_escape').decode("unicode-escape")
-        f.write(jsonStr)
-
-
-async def removemeal(meal: string, campus: int):
-    if system() == "Windows":
-        dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
-    else:
-        dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
-
-    with open(dinePath, 'r', encoding='utf-8') as f:
-        data = json.load(f)
-
-    if data['campus'][campus].count(meal) == 0:
-        await cfg.send("删除失败,菜单没有这个菜喵")
-    else:
-        data['campus'][campus].remove(meal)
-        await cfg.send(f"{meal}已经从{'一区' if campus == 0 else '二区'}删掉了喵!")
-
-    with open(dinePath, 'w', encoding='utf-8') as f:
-        jsonStr = json.dumps(data)
-        jsonStr.encode('unicode_escape').decode("unicode-escape")
-        f.write(jsonStr)
-
-
-async def addmeal_shop(meal: string, shopid: int):
+async def addmeal(meal: string, shopname: string):
     # do
     if system() == "Windows":
         dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
@@ -123,18 +120,18 @@ async def addmeal_shop(meal: string, shopid: int):
     with open(dinePath, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    if data['shop'][shopid].count(meal) > 0:
+    if data[shopname].count(meal) > 0:
         await cfg.send("添加失败,这个菜已经存在了喵")
     else:
-        data['shop'][shopid].append(meal)
-        await cfg.send(f"{meal}已经加到{shop_name[shopid]}了喵!")
+        data[shopname].append(meal)
+        await cfg.send(f"{meal}已经加到{shopname}了喵!")
     with open(dinePath, 'w', encoding='utf-8') as f:
         jsonStr = json.dumps(data)
         jsonStr.encode('unicode_escape').decode("unicode-escape")
         f.write(jsonStr)
 
 
-async def removemeal_shop(meal: string, shopid: int):
+async def removemeal(meal: string, shopname: string):
     # do
     if system() == "Windows":
         dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
@@ -144,11 +141,11 @@ async def removemeal_shop(meal: string, shopid: int):
     with open(dinePath, 'r', encoding='utf-8') as f:
         data = json.load(f)
 
-    if data['shop'][shopid].count(meal) == 0:
+    if data[shopname].count(meal) == 0:
         await cfg.send("删除失败,菜单没有这个菜喵")
     else:
-        data['shop'][shopid].remove(meal)
-        await cfg.send(f"{meal}已经从{shop_name[shopid]}删掉了喵!")
+        data[shopname].remove(meal)
+        await cfg.send(f"{meal}已经从{shopname}删掉了喵!")
 
     with open(dinePath, 'w', encoding='utf-8') as f:
         jsonStr = json.dumps(data)
--- src/plugins/dine.py
+++ src/plugins/dine.py
@@ -13,6 +13,7 @@ from src.libraries.image import *
 from src.libraries.maimai_best_40 import generate
 from src.libraries.maimai_best_50 import generate50
 import re
+
 if system() == "Windows":
     blacklistPath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\blacklist.json"
 else:
@@ -26,10 +27,10 @@ else:
 with open(dinePath, 'r', encoding="utf-8") as f:
     dineList = json.load(f)
 
-dine = on_regex(r"(一|二)区吃什么")
+dine = on_regex(r"^.{2}吃什么$")
 sendDineList = on_regex(r"^.{2}菜单$")
 
-list_name = ["一区", "二区", "超星", "香坊", "哈西", "百盛", "阿城", "江一", "江二"]
+list_name = list(dineList.keys())
 
 
 @sendDineList.handle()
@@ -44,14 +45,9 @@ async def _(event: Event, message: Message = EventMessage()):
     if list_name.count(name) == 0:
         await sendDineList.send(f"根本没有{name}这家店喵...这边建议您吃\n锤子\n锤子\n锤子\n喵!")
     else:
-        index = list_name.index(name)
         listStr = f"群友在{name}登录过的吃的有:\n"
-        if index <= 1:
-            for dish in dineList['campus'][index]:
-                listStr += dish + "           "
-        else:
-            for dish in dineList['shop'][index - 2]:
-                listStr += dish + "           "
+        for dish in dineList[name]:
+            listStr += dish + "           "
         listStr += "喵!"
         await sendDineList.send(listStr)
 
@@ -65,44 +61,44 @@ async def _(event: Event, message: Message = EventMessage()):
 
     with open(dinePath, 'r', encoding="utf-8") as f:
         dineList = json.load(f)
-    campus = 0 if message[0].data['text'][0] == '一' else 1
-    dineInCampus = dineList['campus'][campus]
-    index = random.randint(0, len(dineInCampus))
+    name = event.get_plaintext()[0:2]
+    dineNames = dineList[name]
+    index = random.randint(0, len(dineNames))
     if index == 0:
         await dine.send("吃锤子!")
     else:
-        await dine.send("吃 " + dineInCampus[index - 1] + " 怎么样喵?")
+        await dine.send("吃 " + dineNames[index - 1] + " 怎么样喵?")
     return
 
-
-shop_name = ["超星", "香坊", "哈西", "百盛", "阿城", "江一", "江二"]
-shopDine = on_regex(r"(超星|香坊|阿城|哈西|百盛|江一|江二)吃什么")
-
-
-@shopDine.handle()
-async def _(event: Event, message: Message = EventMessage()):
-    with open(blacklistPath, "r", encoding="utf-8") as f:
-        blackList = json.load(f)
-    qqid = int(event.get_user_id())
-    if qqid in blackList:
-        await shopDine.send("宝宝，你也配用？")
-        return
-    if system() == "Windows":
-        dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
-    else:
-        dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
-
-    with open(dinePath, 'r', encoding="utf-8") as f:
-        dineList = json.load(f)
-    shopName = event.get_plaintext()[0:2]
-    shopid = shop_name.index(shopName)
-    dineInShop = dineList['shop'][shopid]
-    index = random.randint(0, len(dineInShop))
-    if index == 0:
-        await dine.send("吃锤子!")
-    else:
-        await dine.send("吃 " + dineInShop[index - 1] + " 怎么样喵?")
-    return
+#  哪个傻逼写的废品
+# shop_name = ["超星", "香坊", "哈西", "百盛", "阿城", "江一", "江二"]
+# shopDine = on_regex(r"(超星|香坊|阿城|哈西|百盛|江一|江二)吃什么")
+#
+#
+# @shopDine.handle()
+# async def _(event: Event, message: Message = EventMessage()):
+#     with open(blacklistPath, "r", encoding="utf-8") as f:
+#         blackList = json.load(f)
+#     qqid = int(event.get_user_id())
+#     if qqid in blackList:
+#         await shopDine.send("宝宝，你也配用？")
+#         return
+#     if system() == "Windows":
+#         dinePath = "D:\\maimai-bot\\mai-bot-sniper-main\\mai-bot-sniper-main\\prop\\dine.json"
+#     else:
+#         dinePath = "/home/sniperpigeon/bot/azusa-bot/prop/dine.json"
+#
+#     with open(dinePath, 'r', encoding="utf-8") as f:
+#         dineList = json.load(f)
+#     shopName = event.get_plaintext()[0:2]
+#     shopid = shop_name.index(shopName)
+#     dineInShop = dineList['shop'][shopid]
+#     index = random.randint(0, len(dineInShop))
+#     if index == 0:
+#         await dine.send("吃锤子!")
+#     else:
+#         await dine.send("吃 " + dineInShop[index - 1] + " 怎么样喵?")
+#     return
 
 
 # 约饭事件格式为:
